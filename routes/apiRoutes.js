eb6e63c79b1c5a5e01184ecb04e72e957e582cd7
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 15:27:38 -0400

    Retrieve notes from db.json with apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,9 @@
 const router = require("express").Router();
-
+const database = require("../db/db");
 router.get("/notes", (req, res) => {
-      res.send('Hello World!');
-      store
-            .getNotes()
-            .then((notes) => {
-    return res.json(notes);
-  })
-  .catch(
-        (err) => { res.status(500).json(err)}
-        )
+  // Grab the notes list (this should be updated for every new note and deleted note.)
+  console.info(`Retrieving notes from ${database}`);
+  res.json(database);
 });
-
 
 module.exports = router;
