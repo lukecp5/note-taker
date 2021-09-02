05604f65a7ba814506221150700326c58502c9a6
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 16:02:55 -0400

    Pull in req.body as a new note object, add a random number as the id, and push it to the database.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,12 +12,14 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res)=>{
       let jsonFilePath = path.join(__dirname, "/db/db.json");
-      console.info(req.body);
 
+      let note = req.body;
+      note.id = Math.floor(Math.random()*1000);
+      console.info(`Your new note titled: ${note.title} will be saved to the database with an id of ${note.id}`);   
       // This assigns an ID to the newNote, It will be a random number between 1 and 100
       // We push it to db.json.
 
-      // database.push(newNote)
+      database.push(newNote)
 
       // Write the db.json file again.
       // fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {(err)? console.log(err):console.log("Successfully written") });
