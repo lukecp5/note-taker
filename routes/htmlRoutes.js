245450d428ecae037de1c9be7fc9e79a2a2414e7
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 15:05:00 -0400

    Add notes.html to htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/notes.html'));
-})
+});
 
 
 module.exports = router;
