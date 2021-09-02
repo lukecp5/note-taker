da5427140604284a009958652b5faeeb984e7cca
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 15:00:48 -0400

    add module.exports to htmlRoutes, import htmlRoutes to server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,3 +5,6 @@ const router = require('express').Router();
 router.get('/notes', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const db = require("./db/db.json");
 
 const apiRoutes = require("./routes/apiRoutes");
-// const htmlRoutes = require("./routes/htmlRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes");
 // Set up the application to use the Express middleware for routing
 const app = express();
 
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3001;
 // Set up the application to use the public directory for static files
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
+app.use('/', htmlRoutes);
 
 // Tell the application to listen on port 3001, unless a port as an environment variable is specified
 app.listen(PORT, function () {
