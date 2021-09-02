0ce7f03908e0bf8adde50bac05735e9d595a21bf
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 12:50:29 -0400

    Add listen

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+
+
+app.listen(PORT, function() {console.log(`Express server listening on port: ${PORT}`);});
