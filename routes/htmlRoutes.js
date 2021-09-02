e6d7c0f23e21fea192a293ec3825042569ca16f9
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 15:10:14 -0400

    Fix route to notes.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 
 router.get('/notes', (req, res) => {
-      res.sendFile(path.join(__dirname, './public/notes.html'));
+      res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 
