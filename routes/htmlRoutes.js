ef6066841bcd69f0116d7fc3bd0f8d1bd6741bc4
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 15:12:23 -0400

    Fix routes for heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 
 
 router.get('/notes', (req, res) => {
+      console.info(`${req.method} Request to the ${req.path} received`)
       res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
