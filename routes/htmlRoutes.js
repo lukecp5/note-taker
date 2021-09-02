ff2743d5719cace9f1a09201392b569e4d7167ab
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-02 15:16:34 -0400

    Add wildcard router to the html router

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const router = require('express').Router();
 
-
+// Router for the notes page for GET requests
 router.get('/notes', (req, res) => {
       console.info(`${req.method} Request to the ${req.path} received`)
       res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Wildcard router to handle any request that isn't picked up by another router
+router.get('*', (req, res) => {
+      console.info(`${req.method} Request to the ${req.path} received`)
+      res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 
