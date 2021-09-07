d7c802d3abd9f1cee151ca19df920cfc17398e86
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-07 18:31:11 -0400

    Add comments to the apiRoutes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,43 +12,53 @@ router.get("/notes", (req, res) => {
 
 // Uses query parameters to find search through the db.json to find the note id that matches the query. Once if finds that note, it will slice the note from the array of notes and return the new list of notes.
 router.delete("/notes/:id", (req, res) => {
+  // Path to the JSON database for writing to the file. It gets imported at the top as const db
   let jsonPath = path.join(__dirname, "../db/db.json");
-  for(i = 0; i < db.length; i++) {
-    if(db[i].id == req.params.id) {
-      db.splice(i,1);  
+  // For each item in the notes db, check to see if the ID of the delete request matches the id of a note, if It does, delete the note
+  for (i = 0; i < db.length; i++) {
+    if (db[i].id == req.params.id) {
+      db.splice(i, 1);
       break;
-    }else{
-      return "Error: Note not found!"
+    } else {
+      // If the note is not found, return an error
+      return "Error: Note not found!";
     }
   }
-  // Write the newly updated notes to the db.json file. 
-  fs.writeFileSync(jsonPath, JSON.stringify(db), (err)=>
-  {
-    console.info(`Something went wrong while trying to delete the note with ID: ${req.params.id}`);
+  // Write the newly updated notes to the db.json file, after removing the selected note from the database
+  fs.writeFileSync(jsonPath, JSON.stringify(db), (err) => {
+    console.info(
+      `Something went wrong while trying to delete the note with ID: ${req.params.id}`
+    );
     console.error(err);
-  })
-
+  });
+  // Log the deleted note's body to the console, and return the database as JSON to the client
   console.info(`Deleting note ${req.body}`);
   res.json(db);
 });
 
-router.post("/notes", (req, res)=>{
-      let jsonFilePath = path.join(__dirname, "../db/db.json");
+router.post("/notes", (req, res) => {
+  let jsonFilePath = path.join(__dirname, "../db/db.json");
 
-      let note = req.body;
-      note.id = uuid.v4();
-      console.log(`The note's ID is: ${note.id}`);
-      // note.id = Math.floor(Math.random()*1000);
-      console.info(`Your new note titled: ${note.title} will be saved to the database with an id of ${note.id}`);   
-      // This assigns an ID to the newNote, It will be a random number between 1 and 100
-      // We push it to db.json.
+  let note = req.body;
+  note.id = uuid.v4();
+  console.log(`The note's ID is: ${note.id}`);
+  // note.id = Math.floor(Math.random()*1000);
+  console.info(
+    `Your new note titled: ${note.title} will be saved to the database with an id of ${note.id}`
+  );
+  // This assigns an ID to the newNote, It will be a random number between 1 and 100
+  // We push it to db.json.
 
-      db.push(note);
+  db.push(note);
 
-      // Write the db.json file again.
-      fs.writeFile(jsonFilePath, JSON.stringify(db), function (err) {(err)? console.log(err):console.log("Your note has been saved to the database."); });
+  // Write the db.json file again.
+  fs.writeFile(jsonFilePath, JSON.stringify(db), function (err) {
+    err
+      ? console.log(err)
+      : console.log("Your note has been saved to the database.");
+  });
 
-      // Gives back the response, which is the user's new note. 
-      res.json(note);
-})
+  // Gives back the response, which is the user's new note.
+  res.json(note);
+});
 module.exports = router;
